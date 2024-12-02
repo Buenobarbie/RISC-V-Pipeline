67948c48b67e78735732d5f670fd6259f2336419
Buenobarbie <barbarabueno140104@gmail.com>
2024-12-02 09:11:07 -0300

    ✨ Instruction comes from intermediate register

--- RISCV_OAC/processador.v
+++ RISCV_OAC/processador.v
@@ -14,9 +14,11 @@ module polirv #(parameter i_addr_bits = 6, parameter d_addr_bits = 6)
                rf_mux, 
                zero;
 
-    assign opcode = i_mem_data[6:0];
-    assign func3 = i_mem_data[14:12];
-    assign func7b5 = i_mem_data[30];
+    wire [31:0] instruction;
+
+    assign opcode = instruction[6:0];
+    assign func3 = instruction[14:12];
+    assign func7b5 = instruction[30];
 
     uc uc (
         .clk        (clk        ), 
@@ -49,7 +51,8 @@ module polirv #(parameter i_addr_bits = 6, parameter d_addr_bits = 6)
         .i_mem_addr (i_mem_addr ),
         .d_mem_addr (d_mem_addr ),
 
-        .d_mem_data (d_mem_data )
+        .d_mem_data (d_mem_data ),
+        .instruction (instruction)
     );
 
 endmodule
