5179372e913669bd723a855b4b95aa40ee4a997d
Buenobarbie <barbarabueno140104@gmail.com>
2024-12-02 09:10:33 -0300

    ✨ Add enable to pc

--- RISCV_OAC/PC.v
+++ RISCV_OAC/PC.v
@@ -1,5 +1,4 @@
 /*
-
     Program Counter:
 
     Contador que navega entre as instruções a serem executadas.
@@ -9,16 +8,17 @@
 module PC #(parameter N = 64)(
     input wire          clk, 
     input wire          rst, 
+    input wire          enable, // Added enable signal
     input wire  [N-1:0] pc_in, 
     output reg  [N-1:0] pc_out
 );
     
     always @ (posedge clk) begin
-        if(rst) begin
-            pc_out <= {N{1'b0}};
+        if (rst) begin
+            pc_out <= {N{1'b0}}; // Reset PC to 0
         end
-        else begin
-            pc_out <= pc_in; 
+        else if (enable) begin
+            pc_out <= pc_in;     // Update PC only if enable is asserted
         end
     end
 
