c752ed3703ed97b0eae96742845b54a704151dec
Buenobarbie <barbarabueno140104@gmail.com>
2024-12-02 22:42:36 -0300

    🚧 Working on forwarding and stall

--- RISCV_OAC/datapath.v
+++ RISCV_OAC/datapath.v
@@ -4,7 +4,6 @@ module datapath #(parameter i_addr_bits = 6, parameter d_addr_bits = 6) (
     input wire        d_mem_we, 
     input wire        rf_we,
     input wire        ula_src, 
-    input wire        pc_src, 
     input wire        rf_src,
     input wire [2:0]  ula_cmd,
     input wire [31:0] i_mem_data,
@@ -31,6 +30,8 @@ module datapath #(parameter i_addr_bits = 6, parameter d_addr_bits = 6) (
                 ram_out_mux, 
                 imme_shift,
                 add_out_2;
+
+    wire pc_src;
 
 //////////////////////////////////////////////// Instruction fetch ////////////////////////////////////////////////////// 
     
@@ -84,8 +85,8 @@ module datapath #(parameter i_addr_bits = 6, parameter d_addr_bits = 6) (
     assign instruction = reg_IF_ID[31:0];
 
     harzard_detection_unit hazard_detection_unit (
-    .ID_EX_MemRead     (~d_mem_we),  // MemRead é o contrário de d_mem_we
-    .ID_EX_RegisterRd  (reg_IF_ID[11:7] ), 
+    .ID_EX_MemRead     (~reg_ID_EX[277]),  // MemRead é o contrário de d_mem_we
+    .ID_EX_RegisterRd  (reg_ID_EX[132:128] ), 
     .IF_ID_RegisterRs1  (reg_IF_ID[19:15] ), 
     .IF_ID_RegisterRs2  (reg_IF_ID[24:20] ), 
     .hazard_detection_src (hazard_detection_src)
@@ -108,22 +109,24 @@ module datapath #(parameter i_addr_bits = 6, parameter d_addr_bits = 6) (
         .imm_out(imme_out   )
     );
 
-    wire [7:0] control_signals;
-    mux_2x1  #(.N(8))
+    wire [6:0] control_signals;
+    mux_2x1  #(.N(7))
     mux_stall(
-        .d0 (8'b0  ), // Se stall (harzard_detection_unit = 0), os sinais de controle devem ser todos 0
-        .d1 ({d_mem_we, rf_we, ula_cmd, ula_src, branch, rf_src}  ), 
+        .d0 (7'b0  ), // Se stall (harzard_detection_unit = 0), os sinais de controle devem ser todos 0
+        .d1 ({d_mem_we, ula_cmd, ula_src, branch, rf_src}  ), 
         .S  (hazard_detection_src), 
         .Y  (control_signals          )
     );
 
-    wire [300:0] reg_ID_EX;
-    // WB_control_signals: sinais de controle para o estágio de write-back
-    // MEM_control_signals: sinais de controle para o estágio de memória
-    // EX_control_signals: sinais de controle para o estágio de execução
-    // [329:266] rf_out_A: dado do registrador 1
-    // [265:202] rf_out_B: dado do registrador 2
-    // [201:138] regA_addr reg_IF_ID[19:15] : endereço do registrador 1
+    wire [277:0] reg_ID_EX;
+    // [277] d_mem_we: controla a escrita na memória de dados
+    // [276:274]ula_cmd: comando da ula
+    // [273] ula_src: origem da entrada da ula
+    // [272] branch: controla o branch
+    // [271] rf_src: origem do dado que será escrito no registrador
+    // [270:207] rf_out_A: dado do registrador 1
+    // [206:143] rf_out_B: dado do registrador 2
+    // [142:138] regA_addr reg_IF_ID[19:15] : endereço do registrador 1
     // [137:133] regB_addr reg_IF_ID[24:20]: endereço do registrador 2
     // [132:128]reg_IF_ID[11:7] : endereço do registrador de destino
     // [127:64] registrador_IF_ID[pc_out]: endereço da instrução vindo do registrador anterior
@@ -143,12 +146,12 @@ module datapath #(parameter i_addr_bits = 6, parameter d_addr_bits = 6) (
     
     wire [1:0] forwardA, forwardB;
     forwarding_unit forwarding_unit (
-        .EX_MEM_RegWrite    (reg_EX_MEM[197] ), 
-        .EX_MEM_RegisterRd   (reg_EX_MEM[197:193] ), 
-        .ID_EX_RegisterRs1   (reg_ID_EX[127:123] ), 
-        .ID_EX_RegisterRs2   (reg_ID_EX[122:118] ), 
-        .MEM_WB_RegWrite    (reg_MEM_WB[197] ), 
-        .MEM_WB_RegisterRd   (reg_MEM_WB[197:193] ), 
+        .EX_MEM_RegWrite    (reg_EX_MEM[] ), 
+        .EX_MEM_RegisterRd   (reg_EX_MEM[] ), 
+        .ID_EX_RegisterRs1   (reg_ID_EX[] ), 
+        .ID_EX_RegisterRs2   (reg_ID_EX[] ), 
+        .MEM_WB_RegWrite    (reg_MEM_WB[] ), 
+        .MEM_WB_RegisterRd   (reg_MEM_WB[] ), 
         .ForwardA           (forwardA    ), 
         .ForwardB           (forwardB    )
     );
@@ -159,51 +162,57 @@ module datapath #(parameter i_addr_bits = 6, parameter d_addr_bits = 6) (
     wire [63:0] ula_operand1, ula_operand2;
 
     mux_3x1 mux_ula_op1 (
-        .d0 (reg_ID_EX[127:64]  ), // rf_out_A: dado do registrador 2
-        .d1 (reg_EX_MEM[63:0]  ), // imme_out: imediato
-        .d2 (ram_out_mux    ), // dado da memória de dados
+        .d0 (reg_ID_EX[270:207]  ), // rf_out_A: dado do registrador 1
+        .d1 (ram_out_mux  ), 
+        .d2 (  reg_EX_MEM[132:69]  ), 
         .S0  (forwardA[0]    ), 
         .S1  (forwardA[1]    ),
         .Y  (ula_operand1    )
     );
 
     mux_3x1 mux_ula_op2 (
-        .d0 (reg_ID_EX[127:64]  ), // rf_out_B: dado do registrador 2
-        .d1 (reg_ID_EX[63:0]  ), // imme_out: imediato
+        .d0 (reg_ID_EX[206:143]  ), // rf_out_B: dado do registrador 2
+        .d1 (reg_EX_MEM[132:69]   ),
         .d2 (ram_out_mux    ), // dado da memória de dados
         .S0  (forwardB[0]    ), 
         .S1  (forwardB[1]    ),
-        .Y  (ula_in     )
+        .Y  (ula_operand2     )
     );
 
 
     ULA ula (
-        .ula_src    (ula_cmd        ), 
+        .ula_src    (reg_ID_EX[276:274]       ), 
         .operand1   (ula_operand1    ), 
         .operand2   (ula_operand2     ), 
         .result     (ula_out        ), 
         .zero       (zero           )
     );
 
-    //SUS
-    //assign feito para controlar o shift do imediato
-    assign imme_shift = (mem_out[6:0] == 7'b0010111) ? reg_ID_EX[63:0] << 12'b0 : 
-                        (mem_out[6:0] == 7'b1101111) ? reg_ID_EX[63:0] << 1'b0 : reg_ID_EX[63:0] << 1'b1;
+    //                  imme_out
+    assign imme_shift = reg_ID_EX[63:0] << 1'b1;
+
 
     assign d_mem_addr = ula_out;
 
-    wire [197:0] reg_EX_MEM;
-    // reg_ID_EX[300:256]: endereço do registrador de destino
-    // add_out_2: resultado da soma do imediato com o pc
-    // zero: flag de zero
-    // ula_out: resultado da operação da ula
-    // reg_ID_EX[127:64]: dado do registrador 2
+    wire [4:0] rd_addr;
+    assign rd_addr = reg_ID_EX[132:128];
+
+    wire [200:0] reg_EX_MEM;
+    // [200] d_mem_we reg_ID_EX[277]: controla a escrita na memória de dados
+    // [199] branch reg_ID_EX[272]: controla o branch
+    // [198] rf_src reg_ID_EX[271]: origem do dado que será escrito no registrador
+    // [197:134] add_out_2: resultado da soma do imediato com o pc
+    // [133] zero: flag de zero
+    // [132:69] ula_out: resultado da operação da ula
+    // [68:5] reg_ID_EX[206:143]: dado do registrador 2
+    // [4:0]  Rd_addr endereco do registrador de destino
+
 
     RegN registrador_EX_MEM (
         .CLK        (CLK        ), 
         .RESET      (~rst_n     ), 
         .ENABLE     (1'b1       ), 
-        .LOAD       ({reg_ID_EX[300:256], ula_operand2, zero, ula_out, reg_ID_EX[127:64]}    ), 
+        .LOAD       ({reg_ID_EX[277], reg_ID_EX[272], reg_ID_EX[271], add_out_2, zero, ula_out, reg_ID_EX[206:143], rd_addr}    ), 
         .Q          (reg_EX_MEM    )
     );
 
@@ -216,22 +225,26 @@ module datapath #(parameter i_addr_bits = 6, parameter d_addr_bits = 6) (
     assign mem_out = i_mem_data;
 
     //assign feitos para garantir o funcionamento do inout
+                        //d_mem_we           rf_out_B
+    assign d_mem_data = (reg_EX_MEM[200]) ? reg_EX_MEM[197:134] : 64'bz;
+    assign ram_out = (~reg_EX_MEM[200]) ? d_mem_data : 64'bz;
 
-    assign d_mem_data = (d_mem_we) ? reg_EX_MEM[63:0] : 64'bz;
-    assign ram_out = (~d_mem_we) ? d_mem_data : 64'bz;
-
-    wire [132:0] reg_MEM_WB;
-    // reg_EX_MEM[197:193] : endereço do registrador de destino
-    // ram_out: dado da memória de dados
-    // ula_out: resultado da operação da ula
+    wire [133:0] reg_MEM_WB;
+    // [133] rf_we: controla a escrita no registrador
+    // [132:128] rd_addr: endereço do registrador de destino
+    // [127:64] ram_out: dado da memória de dados
+    // [63:0] ula_out: resultado da operação da ula
 
     RegN registrador_MEM_WB (
         .CLK        (CLK        ), 
         .RESET      (~rst_n     ), 
         .ENABLE     (1'b1      ), 
-        .LOAD       ({reg_EX_MEM[rf_src], ram_out, reg_EX_MEM[ula_out]}    ), 
+        .LOAD       ({reg_EX_MEM[198], reg_EX_MEM[4:0], ram_out, reg_EX_MEM[132:69]}    ), 
         .Q          (reg_MEM_WB    )
     );
+
+    // BRANCH
+    assign pc_src = reg_EX_MEM[133] && reg_EX_MEM[199];
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -240,7 +253,7 @@ module datapath #(parameter i_addr_bits = 6, parameter d_addr_bits = 6) (
     mux_2x1 mux_ram (
         .d0 (reg_MEM_WB[63:0]  ), // ula_out: resultado da operação da ula
         .d1 (reg_MEM_WB[127:64]  ),  // dado da memória de dados (ram_out   )
-        .S  (reg_MEM_WB[128]     ),  // rf_src: controla qual dado será escrito no registrador
+        .S  (reg_MEM_WB[133]     ),  // rf_src: controla qual dado será escrito no registrador
         .Y  (ram_out_mux)
     );
 
