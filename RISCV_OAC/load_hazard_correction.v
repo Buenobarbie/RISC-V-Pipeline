0e56a53b5ce7925eeaeb4abeaf1bb58a02fe93e6
juliana.mitiehn <juliana.mitiehn@usp.br>
2024-12-02 22:16:14 -0300

    :sparkles: load hazard correction module

---
+++ RISCV_OAC/load_hazard_correction.v
@@ -0,0 +1,15 @@
+module load_hazard_correction (
+    input [4:0] Rs1D, Rs2D, RdE; 
+    input ResultSrcE0, PCSrcE;
+    output Stall F, StallD, FlushD, FlushE; 
+);
+
+    wire lwStall = ResultSrcE0 && ((Rs1D == RdE) || (Rs2D == RdE));
+
+    StallF = lwStall;
+    StallD = lwStall;
+
+    FlushD = PCSrcE;
+    FlushE = lwStall || PCSrcE;
+    
+endmodule
